dabc1313ec17d6bb39dad47659bde5c751ad773c
WyhwdS <139298157+WyhwdS@users.noreply.github.com>
2023-09-17 18:13:13 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,1 +1,16 @@
-
+#include<stdio.h>
+int gcd(int a,int b) {
+        if(a%b==0) 
+            return b;
+	    else 
+            return (gcd(b,a%b));
+}
+int main()
+{
+    int a[10],b[10];
+    for(int i=0;i<10;i++)
+    {
+        scanf("%d %d",&a[i],&b[i]);
+    }
+    printf("%d",gcd(a[0],b[0]));
+}
